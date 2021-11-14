5ba94ea71c7808061e8ecb1ad4a3566c5de36921
czb <2273735762@qq.com>
2021-11-14 11:08:54 +0800

    测试多人开发2

--- src/main/java/cn/czb/demo/test/Test.java
+++ src/main/java/cn/czb/demo/test/Test.java
@@ -6,5 +6,6 @@ public class Test {
         System.out.println("这是我第一次使用git");
         System.out.println("基于第一次开发");
         System.out.println("基于第一次开发开发");
+        System.out.println("测试demo");
     }
-}
+            }
